bd34c1646f5b970cdbe72874f870cada881cdfef
Michael Lopez <luigixmario11695@gmail.com>
2017-01-18 16:33:30 +0000

    Added contact form mailer file.

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'luigixmario11695@gmail.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
