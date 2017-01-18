ea5100e6bc0a6f9622aedf2c4c5da51dac3d9d83
Michael Lopez <luigixmario11695@gmail.com>
2017-01-18 23:33:48 +0000

    Added username fields for users and admins tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118232325) do
+ActiveRecord::Schema.define(version: 20170118233245) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -25,8 +25,10 @@ ActiveRecord::Schema.define(version: 20170118232325) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_admins_on_username", unique: true
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -50,8 +52,10 @@ ActiveRecord::Schema.define(version: 20170118232325) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
