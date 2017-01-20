3588b0cdd503357e1b3d13f87a15f69ce1a94af9
Michael Lopez <luigixmario11695@gmail.com>
2017-01-20 06:52:52 +0000

    Added avatar column to profiles view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120022930) do
+ActiveRecord::Schema.define(version: 20170120065130) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -43,8 +43,12 @@ ActiveRecord::Schema.define(version: 20170120022930) do
     t.integer  "user_id"
     t.string   "title"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
