31e64ef96245b2d9e75a83dc6548727308116912
Michael Lopez <luigixmario11695@gmail.com>
2017-01-20 19:07:35 +0000

    Secured user pages and added user sindex view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
+  def index
+    @users = User.includes(:profile)
+  end
+  
   # GET to /users/:id
   def show
     @user = User.find( params[:id] )
