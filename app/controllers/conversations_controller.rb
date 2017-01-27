08f9585638850a3361ecd5466be69da2966093c6
Michael Lopez <luigixmario11695@gmail.com>
2017-01-27 16:34:20 +0000

    Allowed admins to see a list of all on site conversations.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,7 @@
 class ConversationsController < ApplicationController
-  before_action :authenticate_user!
-  
   def index
-    @users = User.all
-    @conversations = Conversation.all
+    @users = User.includes(:profile)
+    @conversations = Conversation.all.order("created_at DESC")
   end
   
   def create
